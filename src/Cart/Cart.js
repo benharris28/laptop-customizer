1b5d12ddef65508d93a2d804a11bf5fc8c12b75d
Ben Harris <benjaminharris28@gmail.com>
2020-01-05 16:30:25 -0500

    first commit

---
+++ src/Cart/Cart.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Cart extends React.Component {
+    render() {
+        return (
+            <div>Hello</div>
+        )
+    }
+}
+
+export default Cart;
---
+++ src/OptionCard/OptionCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class OptionCard extends React.Component {
+    render() {
+        return (
+            <div>Hello</div>
+        )
+    }
+}
+
+export default OptionCard;
